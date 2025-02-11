d353418660d7001aed65235b68b9a2abcd5aad8d
SmokePTHD <glapdesousa@gmail.com>
2025-02-11 12:23:42 +0000

    backup

--- src/resolvers/GetUsersByRoleResolver.ts
+++ src/resolvers/GetUsersByRoleResolver.ts
@@ -5,24 +5,24 @@ import { GetUserByRole } from "../dtos/model/GetUserByRoleModel";
 
 @Resolver()
 export class GetUsersByRoleResolver {
-    private firestore = getFirestore();
+  private firestore = getFirestore();
 
-    @Query(() => [GetUserByRole])
-    async getUsersByRole(@Arg("role") role: string): Promise<GetUserByRole[]> {
-        const usersRef = this.firestore.collection("users");
-        const snapshot = await usersRef.where("role", "==", role).get();
+  @Query(() => [GetUserByRole])
+  async getUsersByRole(@Arg("role") role: string): Promise<GetUserByRole[]> {
+    const usersRef = this.firestore.collection("users");
+    const snapshot = await usersRef.where("role", "==", role).get();
 
-        if (snapshot.empty) {
-            return [];
-        }
-
-        const users: GetUserByRole[] = [];
-        snapshot.forEach(doc => {
-            const user = doc.data() as GetUserByRole;
-            user.uid = doc.id;
-            users.push(user);
-        });
-
-        return users;
+    if (snapshot.empty) {
+      return [];
     }
+
+    const users: GetUserByRole[] = [];
+    snapshot.forEach((doc) => {
+      const user = doc.data() as GetUserByRole;
+      user.uid = doc.id;
+      users.push(user);
+    });
+
+    return users;
+  }
 }
